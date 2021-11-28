47cb30ae82a25ef1e95d2d2914371f60769c41b1
Eduardo Rico <erico@intelimetrica.com>
2021-11-28 00:27:03 -0600

    characters module done

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { HttpCode, Injectable, NotFoundException, ServiceUnavailableException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCharacterDto } from './dto/create-character.dto';
 import { UpdateCharacterDto } from './dto/update-character.dto';
+import { Character } from './entities/character.entity';
 
 @Injectable()
 export class CharactersService {
-  create(createCharacterDto: CreateCharacterDto) {
-    return 'This action adds a new character';
+  constructor(
+    @InjectRepository(Character)
+    private characterRepository: Repository<Character>,
+  ) {}
+
+  async create(createCharacterDto: CreateCharacterDto) {
+    try {
+      this.characterRepository.insert(createCharacterDto);
+    } catch (error) {
+      throw new ServiceUnavailableException('I fuck up, Morty');
+    }
   }
 
-  findAll() {
-    return `This action returns all characters`;
+  async findAll(): Promise<CreateCharacterDto[]> {
+    return await this.characterRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} character`;
+  async findOne(id: number): Promise<CreateCharacterDto> {
+    return await this.characterRepository.findOne(id);
   }
 
-  update(id: number, updateCharacterDto: UpdateCharacterDto) {
-    return `This action updates a #${id} character`;
+  async update(id: number, updateCharacterDto: UpdateCharacterDto) {
+    try {
+      await this.characterRepository.update(id, updateCharacterDto);
+    } catch (error) {
+      throw new NotFoundException("I don't know that shit, Morty");
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} character`;
+  async remove(id: number) {
+    try {
+      await this.characterRepository.delete(id);
+    } catch (error) {
+      throw new NotFoundException('Wubba lubba dub dub');
+    }
   }
 }
